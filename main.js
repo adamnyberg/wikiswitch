e4384902742a8c1ec566b1835f1f082708acd53e
Adam Nyberg <adam.nyberg@gmail.com>
2016-04-23 17:15:55 +0200

    feat(switch): toggle between en and sv

---
+++ main.js
@@ -0,0 +1,9 @@
+window.addEventListener("keydown", function(e){
+  if(e.ctrlKey && e.keyCode == 83) {
+    href = document.querySelector(".interwiki-sv a")
+    if (!href) {
+      href = document.querySelector(".interwiki-en a")
+    }
+    window.location.href = href;
+  }
+});
