7918804dc2bbfab94536154b3d4f9772a6525aa2
Adam Nyberg <adam.nyberg@gmail.com>
2016-04-24 13:55:17 +0200

    feat(key binding): use alt instead of ctrl

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 window.addEventListener("keydown", function(e){
-  if(e.ctrlKey && e.keyCode == 83) {
+  if(e.altKey && e.keyCode == 83) {
     href = document.querySelector(".interwiki-sv a")
     if (!href) {
       href = document.querySelector(".interwiki-en a")
